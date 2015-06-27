544ce4a561c4faca1e64ea60168c4f61e123d893
EC2 Default User <ec2-user@ip-172-31-15-132.ap-northeast-1.compute.internal>
2015-06-27 20:04:25 +0000

    add suggestion feature

--- server/controller/suggest.py
+++ server/controller/suggest.py
@@ -19,42 +19,50 @@ def _suggest_result(suggest_type, param):
     return { 'candidates': [], 'description': 'no item' }
   prefix = param['prefix']
   # search here
-  cs = ['Hoge', 'Piyo', 'Fuga', suggest_type]
-  description = 'sample'
-  return { 'type': suggest_type, 'candidates': cs, 'description': description }
-
-
-@get('/{0}/api/suggest'.format(__CONTEXT_ROOT))
-@get('/{0}/api/suggest/name'.format(__CONTEXT_ROOT))
-def suggest_name():
-  result = _suggest_result('name', dict(request.query.decode()))
-  response.content_type = 'application/json'
-  return result 
-
-def _suggest_tag(param):
-  if not param.has_key('prefix'):
-    return { 'type': 'tag', 'candidates': [], 'description': 'no item' }
-  prefix = param['prefix']
-  tab = dynamodb.get_table('tags')
+  tab = dynamodb.get_table(suggest_type)
   items = tab.scan(name_en__beginswith=prefix) 
   scanned = [ dynamodb.item_to_dict(item) for item in items ]
+
   candidates = sorted(scanned, cmp=lambda a,b: cmp(a['name_en'], b['name_en']))[0:5]
+  cand_list = []
+  cand_list = [ c['name_en'] for c in candidates ]
+
+  return cand_list
+
+def _form_suggestions(suggestions):
   return {
-    'type': 'tag', 'candidates': [ c['name_en'] for c in candidates ],
-    'matched': len(scanned),
-    'description': 'found {0} items'.format(len(scanned)),
+    'type': 'name', 'candidates': suggestions ,
+    'matched': len(suggestions),
+    'description': 'found {0} items'.format(len(suggestions)),
   }
+
+@get('/{0}/api/suggest'.format(__CONTEXT_ROOT))
+
+
+@get('/{0}/api/suggest/search'.format(__CONTEXT_ROOT))
+def suggest_search():
+  suggestions = _suggest_result('dishes', dict(request.query.decode()))
+  suggestions.extend(_suggest_result('tags', dict(request.query.decode())))
+  response.content_type = 'application/json'
+  return _form_suggestions(suggestions)
+
+@get('/{0}/api/suggest/name'.format(__CONTEXT_ROOT))
+def suggest_name():
+  suggestions = _suggest_result('dishes', dict(request.query.decode()))
+  response.content_type = 'application/json'
+  return _form_suggestions(suggestions)
 
 @get('/{0}/api/suggest/tag'.format(__CONTEXT_ROOT))
 def suggest_tag():
-  result = _suggest_tag(dict(request.query.decode()))
+  suggestions = _suggest_result('tags', dict(request.query.decode()))
   response.content_type = 'application/json'
-  return result 
+  return _form_suggestions(suggestions)
+
 
 @get('/{0}/api/suggest/restaurant'.format(__CONTEXT_ROOT))
 def suggest_restaurant():
-  result = _suggest_result('restaurant', dict(request.query.decode()))
+  suggestions = _suggest_result('restaurant', dict(request.query.decode()))
   response.content_type = 'application/json'
-  return result 
+  return _form_suggestions(suggestions)
 
 
