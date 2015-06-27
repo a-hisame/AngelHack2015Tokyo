0b853a16434ba84cd355d21be8b28585178ac005
a-hisame <a.hisame@gmail.com>
2015-06-27 19:27:22 +0900

    server will output access log

--- server/run.sh
+++ server/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -d --name server -p 80:8080 \
-  -v /var/log/server:/var/log \
+  -v /var/log/server:/var/log/server \
   -v /etc/localtime:/etc/localtime:ro \
   a-hisame/server:1.0
 
