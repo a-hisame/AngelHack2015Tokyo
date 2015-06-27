c362e9c9ebd995a049bee523dfa5bee1cbfa4590
a-hisame <a.hisame@gmail.com>
2015-06-27 16:34:26 +0900

    add suggestion api

--- server/controller/index.py
+++ server/controller/index.py
@@ -4,11 +4,30 @@
 '''
 '''
 
+import json
 import bottle
 from bottle import route, run, view, static_file, template
-from bottle import get, post, request
+from bottle import get, post, request, response
 
 __CONTEXT_ROOT = 'hungry'
+
+def _suggest_result(p):
+  ''' return suggest json structure '''
+  if not p.has_key('prefix'):
+    return { 'candidates': [], 'description': 'no item' }
+  prefix = p['prefix']
+  # search here
+  cs = ['Hoge', 'Piyo', 'Fuga']
+  description = 'sample'
+  return { 'candidates': cs, 'description': description }
+
+
+@get('/{0}/api/suggest'.format(__CONTEXT_ROOT))
+def suggest():
+  result = _suggest_result(dict(request.query.decode()))
+  response.content_type = 'application/json'
+  return result 
+
 
 @get('/{0}'.format(__CONTEXT_ROOT))
 @get('/{0}/index'.format(__CONTEXT_ROOT))
@@ -16,4 +35,5 @@ __CONTEXT_ROOT = 'hungry'
 def index():
   ''' return index view '''
   return {}
+
 
